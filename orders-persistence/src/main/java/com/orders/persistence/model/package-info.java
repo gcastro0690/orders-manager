4883cd7f1d4237852033167004b24be036a6fef3
Gonzalo Castro <gonzalo.castro@arnaldocastro.com.uy>
2018-04-07 19:23:33 -0300

    complete commit

--- orders-persistence/src/main/java/com/orders/persistence/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Raiz de los packages que contienen las clases del modelo a persistir.
- * Este paquete contiene clases comunes a ser utilizadas por el modelo.
- * 
- */
-package com.orders.persistence.model;
